5bceee93c34c44cb1b90758a0bf397c1c6a39921
majestic1990 <carlosalbertogonzalezbravo@gmail.com>
2026-04-20 21:16:22 -0500

    feat: command engine implemented

---
+++ core/engine.py
@@ -0,0 +1,12 @@
+class Engine:
+    def __init__(self):
+        self.commands = {}
+
+    def register_command(self, name, func):
+        self.commands[name] = func
+
+    def execute(self, command):
+        if command in self.commands:
+            return self.commands[command]()
+        else:
+            return "Comando no reconocido"

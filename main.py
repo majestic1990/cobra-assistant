11818ebc408aa287a03805d05f25f1840669c55e
majestic1990 <carlosalbertogonzalezbravo@gmail.com>
2026-04-21 21:26:40 -0500

    fix: added missing os import for system commands

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from core.engine import Engine
-
+import os
 def saludo():
     return "Hola, soy C.O.B.R.A."
-
+def hora():
+    return os.popen("date").read()
 def main():
     engine = Engine()
 
     # Registrar comando
     engine.register_command("saludo", saludo)
-
+    engine.register_command("hora", hora)
     while True:
         comando = input(">> ").lower()
 
@@ -21,3 +22,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+import os
